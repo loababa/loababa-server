4999fadd9a1b774a3259de6a8c9f3702b561ede3
sangminnim <sangmins930@gmail.com>
2024-05-27 00:16:09 +0900

    initial commit

---
+++ src/main/java/com/loababa/api/LoababaApplication.java
@@ -0,0 +1,13 @@
+package com.loababa.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoababaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoababaApplication.class, args);
+    }
+
+}
