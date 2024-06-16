d69505c1f11dc1443b0b782f35d9aa06250d783f
sangminnim <sangmins930@gmail.com>
2024-06-16 15:41:34 +0900

    refactor: OAuthController 삭제

--- src/main/java/com/loababa/api/auth/ui/AuthController.java
+++ src/main/java/com/loababa/api/auth/ui/AuthController.java
@@ -1,16 +1,25 @@
 package com.loababa.api.auth.ui;
 
 import com.loababa.api.auth.domain.AuthService;
+import com.loababa.api.auth.domain.OAuthService;
 import com.loababa.api.auth.domain.impl.model.AuthToken;
+import com.loababa.api.auth.domain.impl.model.OAuthCredential;
+import com.loababa.api.auth.domain.impl.model.OAuthPlatform;
 import com.loababa.api.auth.domain.impl.model.RefreshToken;
 import com.loababa.api.auth.ui.dto.AuthTokenRefreshRequest;
 import com.loababa.api.common.model.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Auth", description = "인증/인가 관련 API")
@@ -19,6 +28,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+    private final OAuthService oAuthService;
+
+    @Operation(description = "소셜 로그인")
+    @GetMapping("/api/v1/oauth/{platform}")
+    public ApiResponse<AuthToken> requestAuthentication(
+            @Schema(description = "OAuth 플랫폼")
+            @PathVariable
+            @Valid @NotBlank @Pattern(regexp = "^(kakao|google)$") String platform,
+            @Schema(description = "OAuth 토큰을 받기 위한 인가 코드")
+            @RequestParam
+            @Valid @NotBlank String code
+    ) {
+        OAuthPlatform oAuthPlatform = OAuthPlatform.from(platform);
+        var authToken = oAuthService.authenticate(new OAuthCredential(oAuthPlatform, code));
+        return ApiResponse.success(authToken);
+    }
 
     @PostMapping("/api/v1/auth/refresh")
     public ApiResponse<AuthToken> requestTokenRefresh(
--- src/test/java/com/loababa/api/auth/ui/AuthControllerTest.java
+++ src/test/java/com/loababa/api/auth/ui/AuthControllerTest.java
@@ -1,20 +1,30 @@
 package com.loababa.api.auth.ui;
 
 import com.loababa.api.auth.domain.AuthService;
+import com.loababa.api.auth.domain.OAuthService;
+import com.loababa.api.auth.domain.impl.model.AccessToken;
+import com.loababa.api.auth.domain.impl.model.AccessTokenFixtures;
 import com.loababa.api.auth.domain.impl.model.AuthToken;
 import com.loababa.api.auth.domain.impl.model.AuthTokenFixtures;
+import com.loababa.api.auth.domain.impl.model.OAuthCredential;
+import com.loababa.api.auth.domain.impl.model.OAuthPlatform;
 import com.loababa.api.auth.domain.impl.model.RefreshToken;
+import com.loababa.api.auth.domain.impl.model.RefreshTokenFixtures;
 import com.loababa.api.auth.ui.dto.AuthTokenRefreshRequest;
 import com.loababa.api.auth.ui.dto.AuthTokenRefreshRequestFixtures;
 import com.loababa.api.common.ControllerTestBase;
 import com.loababa.api.common.model.ApiResponse;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
 
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,7 +33,64 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class AuthControllerTest extends ControllerTestBase {
 
     @MockBean
+    private OAuthService oAuthService;
+
+    @MockBean
     private AuthService authService;
+
+    @Nested
+    class OAuth_플랫폼_유효성_검사를_할_수_있다 {
+
+        @ParameterizedTest
+        @ValueSource(strings = {"kakao", "google"})
+        void 정상요청(String platform) throws Exception {
+            // given
+            String code = "OAuth 토큰 인가 코드";
+            AccessToken accessToken = AccessTokenFixtures.newAccessToken();
+            RefreshToken refreshToken = RefreshTokenFixtures.newRefreshToken();
+            AuthToken authToken = new AuthToken(accessToken, refreshToken);
+
+            OAuthPlatform oAuthPlatform = OAuthPlatform.from(platform);
+            given(oAuthService.authenticate(
+                    new OAuthCredential(oAuthPlatform, code)
+            )).willReturn(authToken);
+
+            // when
+            ResultActions resultActions = mvc.perform(
+                    get("/api/v1/oauth/{platform}", platform)
+                            .queryParam("code", code)
+            );
+
+            // then
+            String expectedResponseBody = om.writeValueAsString(
+                    ApiResponse.success(authToken)
+            );
+
+            resultActions.andExpect(
+                            status().isOk()
+                    )
+                    .andExpect(
+                            content().json(expectedResponseBody)
+                    );
+        }
+
+        @Test
+        void 지원하지_않은_플랫폼을_입력했을_때() throws Exception {
+            // given
+            String code = "OAuth 토큰 인가 코드";
+            String platform = "notSupportedPlatform";
+
+            // when
+            ResultActions resultActions = mvc.perform(
+                    get("/api/v1/oauth/{platform}", platform)
+                            .queryParam("code", code)
+            );
+
+            // then
+            resultActions.andExpect(status().is4xxClientError());
+        }
+
+    }
 
     @Test
     void 토큰_재발급_요청을_할_수_있다() throws Exception {
