8834791fa98ad2767e4fd09305869285fab97dec
sangminnim <sangmins930@gmail.com>
2024-06-21 02:21:36 +0900

    feat: 예상치 못한 예외 발생 시, 알림 전송 구현

--- src/main/java/com/loababa/api/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/loababa/api/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.loababa.api.common.exception;
 
 import com.loababa.api.common.model.ApiResponse;
+import com.loababa.api.common.service.impl.MessageSender;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -14,9 +16,20 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @Slf4j
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
 
+    private final MessageSender messageSender;
+
     // DEFAULT
+
+    @ResponseStatus(BAD_REQUEST)
+    @ExceptionHandler(RuntimeException.class)
+    public ApiResponse<Void> handleRuntimeException(RuntimeException e) {
+        messageSender.sendErrorNotification("오류가 발생했습니다: " + e.getMessage());
+        log.error(e.getMessage(), e);
+        return ApiResponse.fail("NOT_DEFINED_ERROR", "관리자에게 문의하세요.");
+    }
 
     @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -46,7 +59,7 @@ public class GlobalExceptionHandler {
 
     @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(LoababaBadRequestException.class)
-    public ApiResponse<Void> handleLoababaException(LoababaBadRequestException e) {
+    public ApiResponse<Void> handleLoababaBadRequestException(LoababaBadRequestException e) {
         log.debug(e.getMessage(), e);
         return ApiResponse.fail(e.getClientCode(), e.getClientMessage());
     }
