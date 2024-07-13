6d6b0f12b56e7bfa1fbd143738d49a07fcff2bbf
sangminnim <sangmins930@gmail.com>
2024-07-13 16:08:37 +0900

    fix: JPA 연속된 대문자 변수명에 매칭 안 되는 문제 해결

--- src/main/java/com/loababa/api/auth/infra/persistance/repository/MemberJpaRepository.java
+++ src/main/java/com/loababa/api/auth/infra/persistance/repository/MemberJpaRepository.java
@@ -2,10 +2,17 @@ package com.loababa.api.auth.infra.persistance.repository;
 
 import com.loababa.api.auth.infra.persistance.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MemberJpaRepository extends JpaRepository<MemberEntity, Long> {
 
     boolean existsByNickname(String nickname);
 
+    @Query("""
+            SELECT m.id
+            FROM MemberEntity m
+            WHERE m.oAuthUserId = :oAuthUserId
+            """)
     Long findIdByOAuthUserId(Long oAuthUserId);
+
 }
