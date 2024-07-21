d866eb541c15b1e3013b0a2231e9f52f88d964c7
sangminnim <sangmins930@gmail.com>
2024-07-21 23:24:59 +0900

    test: spring context 불러오기 수정

--- src/test/java/com/loababa/api/LoababaApplicationTest.java
+++ src/test/java/com/loababa/api/LoababaApplicationTest.java
@@ -2,11 +2,9 @@ package com.loababa.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@ActiveProfiles("test")
 @TestPropertySource(value = "classpath:secret/.env")
 class LoababaApplicationTest {
 
