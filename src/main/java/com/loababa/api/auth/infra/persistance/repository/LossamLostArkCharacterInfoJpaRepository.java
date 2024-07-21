24650e3552f97be7077cd800c87f94ab4bfd2b48
sangminnim <sangmins930@gmail.com>
2024-07-21 22:32:06 +0900

    chore: 실수로 인한 패키지 위치 변경

--- src/main/java/com/loababa/api/auth/infra/persistance/repository/LossamLostArkCharacterInfoJpaRepository.java
+++ src/main/java/com/loababa/api/auth/infra/persistance/repository/LossamLostArkCharacterInfoJpaRepository.java
@@ -1,5 +1,6 @@
-package com.loababa.api.auth.infra.persistance.entity;
+package com.loababa.api.auth.infra.persistance.repository;
 
+import com.loababa.api.auth.infra.persistance.entity.LostArkCharacterInfoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LossamLostArkCharacterInfoJpaRepository extends JpaRepository<LostArkCharacterInfoEntity, Long> {
